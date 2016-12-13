9cc3fded71450bfd1ccc6da38ac22180066aa344
Denys G. Santos <denys.santos@engdb.com.br>
2016-12-13 14:00:31 -0200

    Continuous integration

--- src/test/java/org/cmis/cpa/parser/ConnectionParserTest.java
+++ src/test/java/org/cmis/cpa/parser/ConnectionParserTest.java
@@ -1,6 +1,7 @@
 package org.cmis.cpa.parser;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -8,8 +9,14 @@ import static org.junit.Assert.*;
  * Created by gsdenys on 13/12/16.
  */
 public class ConnectionParserTest {
+
     @Before
     public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void teste(){
 
     }
 
